63153cec58423fdb7758452e2402c0c8b999103e
Doron Fletcher <doron.bsg@gmail.com>
2025-12-22 22:20:28 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -4,9 +4,9 @@ import BusinessOnboarding from './pages/BusinessOnboarding';
 import BusinessProfile from './pages/BusinessProfile';
 import Categories from './pages/Categories';
 import CategoryPage from './pages/CategoryPage';
+import Home from './pages/Home';
 import Search from './pages/Search';
 import WriteReview from './pages/WriteReview';
-import HomeV2 from './pages/HomeV2';
 import __Layout from './Layout.jsx';
 
 
@@ -17,9 +17,9 @@ export const PAGES = {
     "BusinessProfile": BusinessProfile,
     "Categories": Categories,
     "CategoryPage": CategoryPage,
+    "Home": Home,
     "Search": Search,
     "WriteReview": WriteReview,
-    "HomeV2": HomeV2,
 }
 
 export const pagesConfig = {
