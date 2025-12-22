46fecd22d3c6124b4dcbb37d520ed1a8c2a9a7a9
Doron Fletcher <doron.bsg@gmail.com>
2025-12-22 22:22:03 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -4,7 +4,6 @@ import BusinessOnboarding from './pages/BusinessOnboarding';
 import BusinessProfile from './pages/BusinessProfile';
 import Categories from './pages/Categories';
 import CategoryPage from './pages/CategoryPage';
-import Home from './pages/Home';
 import Search from './pages/Search';
 import WriteReview from './pages/WriteReview';
 import __Layout from './Layout.jsx';
@@ -17,7 +16,6 @@ export const PAGES = {
     "BusinessProfile": BusinessProfile,
     "Categories": Categories,
     "CategoryPage": CategoryPage,
-    "Home": Home,
     "Search": Search,
     "WriteReview": WriteReview,
 }
