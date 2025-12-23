3d3f760334ff1df4df913eb15d0d37077b50de17
Doron Fletcher <doron.bsg@gmail.com>
2025-12-23 02:38:56 +0000

    File changes

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -301,7 +301,7 @@ export default function Events() {
             className="h-[calc(100vh-140px)] w-full z-0 relative"
           >
             <TileLayer
-              url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+              url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
             />
             {filteredEvents.map((event) => (
